a0fdfe8033e87eafe8780d5603c5ce5567d41a40
Eric <54079742+Eric-programming@users.noreply.github.com>
2021-05-22 11:24:23 -0700

    Update BinaryTreePreorderTraversal_Recursive.java

--- src/Tree/BinaryTreePreorderTraversal/BinaryTreePreorderTraversal_Recursive.java
+++ src/Tree/BinaryTreePreorderTraversal/BinaryTreePreorderTraversal_Recursive.java
@@ -1,24 +1,16 @@
 class Solution {
+    List<Integer> res = new LinkedList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new LinkedList<>();
-        if(root == null) return res;
-        Stack<TreeNode> stack = new Stack<>();
-
-       stack.add(root);
-
-        while(stack.isEmpty() == false){
-            TreeNode top = stack.pop();
-            //add the current node 
-            res.add(top.val);
-            if(top.right != null){
-                //ensure that right is at the bottom of the left
-                stack.add(top.right);
-            }
-            if(top.left != null){
-                //ensure that left is on the top
-                stack.add(top.left);
-            }
-        }
+        dfs(root);
         return res;
+    }
+    private void dfs(TreeNode root){
+        if(root == null) return;
+        //First add current element
+        res.add(root.val);
+        //traverse left side
+        dfs(root.left);
+        //traverse right side
+        dfs(root.right);
     }
 }
