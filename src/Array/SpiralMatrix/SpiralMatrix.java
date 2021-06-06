94964df4697129df364d8ef98800021e6cad3793
Eric <54079742+Eric-programming@users.noreply.github.com>
2021-06-06 11:47:50 -0700

    Create SpiralMatrix.java

---
+++ src/Array/SpiralMatrix/SpiralMatrix.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {        
+        //Define borders
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = n - 1, top = 0, down = m - 1;
+        
+        //Define result arr
+        Integer[] res = new Integer[m * n];
+        int index = 0;
+        
+        while(index < res.length){
+            //Traverse left
+            for(int i = left; i <= right && index < res.length; i++){
+                res[index++] = matrix[top][i];
+            }
+            top++;
+            //Traverse down
+            for(int i = top; i <= down && index < res.length; i++){
+                res[index++] = matrix[i][right];
+            }
+            right--;
+            //Traverse right
+            for(int i = right; i >= left && index < res.length; i--){
+                res[index++] = matrix[down][i];
+            }
+            down--;
+            //Traverse top
+            for(int i = down; i >= top && index < res.length; i--){
+                res[index++] = matrix[i][left];
+            }
+            left++;
+        }
+        return Arrays.asList(res);
+    }
+}
