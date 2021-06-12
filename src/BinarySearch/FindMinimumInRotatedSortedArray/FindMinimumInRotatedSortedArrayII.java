17e7a9866b6ebd3381b7a1fcc94ab1056c2396f7
Eric <54079742+Eric-programming@users.noreply.github.com>
2021-06-12 11:07:07 -0700

    Create FindMinimumInRotatedSortedArrayII.java

---
+++ src/BinarySearch/FindMinimumInRotatedSortedArray/FindMinimumInRotatedSortedArrayII.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMin(int[] nums) {
+        //define pointers
+        int L = 0, N = nums.length, R = N - 1;
+        //base case: if array is already sorted & not rotated
+        if(nums[L] < nums[R]) return nums[L];
+        
+        //binary search
+        while(L + 1 < R){
+            int mid = (L + R)/2;
+            //1. if nums[mid] <= nums[R] then min is at the left side
+            if(nums[mid] <= nums[R]){
+                R--;
+            }
+            //2. if nums[mid] > nums[R] then min is at the right side
+            else{
+                L++;
+            }
+        }
+        //return the smallest value between nums[L] and nums[R]
+        return Math.min(nums[R],nums[L]);
+    }
+}
