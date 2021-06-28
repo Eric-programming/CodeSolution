29987ac9bab459b1e06be21287c514a7fbd0fda2
Eric <54079742+Eric-programming@users.noreply.github.com>
2021-06-28 14:51:16 -0700

    Create NumberOfProvinces.java

---
+++ src/Search/NumberOfProvinces/NumberOfProvinces.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        UnionFind uf = new UnionFind(n);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j || isConnected[i][j] == 0) continue;
+                uf.union(i, j);
+            }
+        }
+        return uf.numOfComponets;
+    }
+}
+public class UnionFind {
+    private int[] parents;
+    private int[] size;
+    int numOfComponets = 0;
+
+    public UnionFind(int n) {
+        parents = new int[n];
+        size = new int[n];
+        numOfComponets = n;
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int cur) {
+        int root = cur;
+        while (root != parents[root]) {
+            root = parents[root];
+        }
+        // Path Compression
+        while (cur != root) {
+            int preParent = parents[cur];
+            parents[cur] = root;
+            cur = preParent;
+        }
+        return root;
+    }
+
+    public int findComponentSize(int cur) {
+        int parent = find(cur);
+        return size[parent];
+    }
+
+    public void union(int node1, int node2) {
+        int node1Parent = find(node1);
+        int node2Parent = find(node2);
+
+        if (node1Parent == node2Parent)
+            return;
+
+        if (size[node1Parent] > size[node2Parent]) {
+            parents[node2Parent] = node1Parent;
+            size[node1Parent] += size[node2Parent];
+        } else {
+            parents[node1Parent] = node2Parent;
+            size[node2Parent] += size[node1Parent];
+        }
+        numOfComponets--;
+    }
+}
