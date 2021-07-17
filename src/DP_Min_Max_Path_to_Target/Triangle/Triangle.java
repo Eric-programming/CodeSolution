94b588cc71f0ebc2c66ab6a028b9e1c14535d408
Eric <54079742+Eric-programming@users.noreply.github.com>
2021-07-16 18:26:15 -0700

    Update Triangle.java

--- src/DP_Min_Max_Path_to_Target/Triangle/Triangle.java
+++ src/DP_Min_Max_Path_to_Target/Triangle/Triangle.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_Bottle_up {
     public int minimumTotal(List<List<Integer>> triangle) {
         int N = triangle.size();
         for(int row = N - 2; row >= 0; row--){
@@ -11,5 +11,40 @@ class Solution {
             }
         }
         return triangle.get(0).get(0);
+    }
+}
+class Solution_Top_Down {
+    List<List<Integer>> triangle;
+    List<Map<Integer, Integer>> cache;
+    
+    public int minimumTotal(List<List<Integer>> triangle) {
+        this.triangle = triangle;
+        //Define cache
+        cache = new ArrayList<>();
+        int N = triangle.size();
+        for(int i = 0; i < N; i++){
+            cache.add(new HashMap<>());
+        }
+        
+        return helper(0, 0);
+    }
+    private int helper(int row, int col){
+        int curVal = triangle.get(row).get(col);
+        
+        //Return current element if we are at the last row
+        if(row == triangle.size() - 1) return curVal;
+        
+        Map<Integer, Integer> map = cache.get(row);
+        
+        //Check if we pre computed this column before
+        if(map.containsKey(col)) return map.get(col);
+        
+        //Find the minimum path sum for next rows
+        int path1 = helper(row + 1, col);
+        int path2 = helper(row + 1, col + 1);
+        
+        //cache the computed result
+        map.put(col, Math.min(path1, path2) + curVal);
+        return map.get(col);
     }
 }
