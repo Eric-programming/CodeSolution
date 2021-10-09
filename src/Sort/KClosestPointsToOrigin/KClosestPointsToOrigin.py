a187d9ee801940abc614bf8bc326d06cf7d180f2
Eric <54079742+Eric-programming@users.noreply.github.com>
2021-10-09 12:53:12 -0700

    Create KClosestPointsToOrigin.py

---
+++ src/Sort/KClosestPointsToOrigin/KClosestPointsToOrigin.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        
+        heap = []
+        
+        for (x, y) in points:
+            dist = -(x*x + y*y)
+            if len(heap) == K:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+        
+        return [(x,y) for (dist,x, y) in heap]
