743e979b46934e13be4ef32105e768a0c38cc38c
Eric-programming <eric09279@outlook.com>
2021-10-21 17:19:14 -0700

    reformat

--- src/Array/ReorderDataInLogFiles/ReorderDataInLogFiles.java
+++ src/Array/ReorderDataInLogFiles/ReorderDataInLogFiles.java
@@ -1,24 +1,34 @@
+import java.util.Collection;
+import java.util.Collections;
+
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        Arrays.sort(logs, (log1, log2)->{
+        Arrays.sort(logs, (log1, log2) -> {
             String[] arr1 = splitStr(log1);
             String[] arr2 = splitStr(log2);
-            
+
             boolean isNum1 = isNumber(arr1[1].charAt(0));
             boolean isNum2 = isNumber(arr2[1].charAt(0));
-            if(isNum1 && isNum2) return 0;
-            else if(isNum1) return 1;
-            else if (isNum2) return -1;
+            if (isNum1 && isNum2)
+                return 0;
+            else if (isNum1)
+                return 1;
+            else if (isNum2)
+                return -1;
             boolean isSameContent = arr1[1].equals(arr2[1]);
-            if(isSameContent) return arr1[0].compareTo(arr2[0]);
+            if (isSameContent)
+                return arr1[0].compareTo(arr2[0]);
             return arr1[1].compareTo(arr2[1]);
         });
+        Collections.sort(list);
         return logs;
     }
-    private String[] splitStr(String log){
+
+    private String[] splitStr(String log) {
         return log.split(" ", 2);
     }
-    private boolean isNumber(char curChar){
-        return  '0' <= curChar && curChar <= '9';
+
+    private boolean isNumber(char curChar) {
+        return '0' <= curChar && curChar <= '9';
     }
 }
