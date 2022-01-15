a700445434b2840713b8533314ce92a2f65a53c8
Eric <54079742+Eric-programming@users.noreply.github.com>
2022-01-14 20:56:42 -0800

    Update and rename OpenClosed_Good_Example.java to Solution.cs

---
+++ src/SOLID_Principles/OpenClosed/Solution.cs
@@ -0,0 +1,19 @@
+abstract class Pen
+{
+    public void write()
+    {
+
+    }
+    public void earse()
+    {
+
+    }
+}
+
+class Highlighter : Pen
+{
+    public void highLightThings()
+    {
+
+    }
+}
