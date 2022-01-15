a31def34b35fe9d1dc17f3333d446a32b1499c08
Eric <54079742+Eric-programming@users.noreply.github.com>
2022-01-14 21:55:41 -0800

    Update Solution.cs

--- src/SOLID_Principles/DependencyInversion/Solution.cs
+++ src/SOLID_Principles/DependencyInversion/Solution.cs
@@ -1,44 +1,44 @@
 class OnlineStore
-        {
-            private IPaymentProcessor paymentProcessor;
+{
+    private IPaymentProcessor paymentProcessor;
 
-            public OnlineStore(IPaymentProcessor paymentProcessor)
-            {
-                this.paymentProcessor = paymentProcessor;
-            }
+    public OnlineStore(IPaymentProcessor paymentProcessor)
+    {
+        this.paymentProcessor = paymentProcessor;
+    }
 
-            public void makeAPaymentForAComputer(int quantity)
-            {
-                this.paymentProcessor.makeAPayment(quantity * 10);
-            }
-        }
+    public void makeAPaymentForAComputer(int quantity)
+    {
+        this.paymentProcessor.makeAPayment(quantity * 10);
+    }
+}
 
-        interface IPaymentProcessor
-        {
-            public void makeAPayment(int total);
-        }
+interface IPaymentProcessor
+{
+    public void makeAPayment(int total);
+}
 
-        class Paypal : IPaymentProcessor
-        {
-            public void makeAPayment(int total)
-            {
-                this.makeAPaymentWithPaypal(total);
-            }
+class Paypal : IPaymentProcessor
+{
+    public void makeAPayment(int total)
+    {
+        this.makeAPaymentWithPaypal(total);
+    }
 
-            public void makeAPaymentWithPaypal(int val)
-            {
+    public void makeAPaymentWithPaypal(int val)
+    {
 
-            }
-        }
-        class Stripe : IPaymentProcessor
-        {
-            public void makeAPayment(int total)
-            {
-                this.makeAPaymentWithStripe(total);
-            }
+    }
+}
+class Stripe : IPaymentProcessor
+{
+    public void makeAPayment(int total)
+    {
+        this.makeAPaymentWithStripe(total);
+    }
 
-            public void makeAPaymentWithStripe(int val)
-            {
+    public void makeAPaymentWithStripe(int val)
+    {
 
-            }
-        }
+    }
+}
