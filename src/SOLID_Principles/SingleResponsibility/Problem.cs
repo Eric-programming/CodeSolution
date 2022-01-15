26fbd72d932b1039c8ddbbaa0535691a86055158
Eric <54079742+Eric-programming@users.noreply.github.com>
2022-01-14 20:39:19 -0800

    Update and rename SingleResponsibility_Bad_Example.java to Problem.cs

---
+++ src/SOLID_Principles/SingleResponsibility/Problem.cs
@@ -0,0 +1,22 @@
+class Product
+{
+    public int price;
+    public string productName;
+    public Product(int price, string productName)
+    {
+        this.price = price;
+        this.productName = productName;
+    }
+    public void CalculateGST()
+    {
+
+    }
+    public void CalculatePST()
+    {
+
+    }
+    public void CalculateDiscount()
+    {
+
+    }
+}
